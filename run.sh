5b71b9281a6321133c13a554e873b410af29b5a4
THOCED <thonon_cedric@hotmail.com>
2015-04-06 23:46:23 +0200

    commit run.sh

---
+++ run.sh
@@ -0,0 +1,7 @@
+
+echo "Lancement du jeu SuperRobot !!!!"
+java -Xmx256m -cp ./lib/jsfml.jar:./lib/jbox2d-library.jar:./lib/jdom-2.0.5.jar:./lib/javax.json-1.0.2.jar:bin/ bilou.mainProgram
+echo "SuperRobot terminé!"
+
+
+
